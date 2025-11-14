15d8657d8bb82dc31c9f75d87596f2e4e1808779
mervedyasa <m.mrvdnmz@gmail.com>
2025-11-14 17:20:08 +0300

    Add files via upload

---
+++ luminaire_data.js
@@ -0,0 +1,818 @@
+const luminaireData = {
+  "60X60": [
+    {
+      "NO": 1,
+      "FIRM": "REGENT",
+      "MODEL": "Geo 3 Office - 600X600-31W",
+      "DEGREE": 60.0,
+      "LM": 4100,
+      "W": 31
+    },
+    {
+      "NO": 2,
+      "FIRM": "REGENT",
+      "MODEL": "Geo 3 Office - 600X600-23W",
+      "DEGREE": 60.0,
+      "LM": 3080,
+      "W": 23
+    },
+    {
+      "NO": 3,
+      "FIRM": "REGENT",
+      "MODEL": "Dime Office-630X630 - 43W",
+      "DEGREE": 60.0,
+      "LM": 4402,
+      "W": 43
+    },
+    {
+      "NO": 4,
+      "FIRM": "XAL",
+      "MODEL": "INTO 625 ",
+      "DEGREE": 60.0,
+      "LM": 4560,
+      "W": 29
+    },
+    {
+      "NO": 5,
+      "FIRM": "REGENT",
+      "MODEL": "Item Office-700X700-34W",
+      "DEGREE": 90.0,
+      "LM": 3700,
+      "W": 34
+    },
+    {
+      "NO": 6,
+      "FIRM": "REGENT",
+      "MODEL": "Item Office-700X700-44W",
+      "DEGREE": 90.0,
+      "LM": 4600,
+      "W": 44
+    },
+    {
+      "NO": 7,
+      "FIRM": "FAGERHULT",
+      "MODEL": "22255-402 Dwide ceiling",
+      "DEGREE": 110.0,
+      "LM": 4366,
+      "W": 33
+    },
+    {
+      "NO": 8,
+      "FIRM": "FAGERHULT",
+      "MODEL": "23207-402 Multilume Slim Opal",
+      "DEGREE": 120.0,
+      "LM": 3421,
+      "W": 35
+    },
+    {
+      "NO": 9,
+      "FIRM": "PROLICHT",
+      "MODEL": "SMOOTHY 60X60",
+      "DEGREE": 120.0,
+      "LM": 5271,
+      "W": 45
+    },
+    {
+      "NO": 10,
+      "FIRM": "I GUZZINI ",
+      "MODEL": "i PLAN ACCESS SQUARE",
+      "DEGREE": 120.0,
+      "LM": 3100,
+      "W": 33
+    }
+  ],
+  "Downlight": [
+    {
+      "NO": 1,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "iMAX II ROUND ADJUSTABLE CL 9306 W",
+      "DEGREE": 6.0,
+      "LM": 274,
+      "W": 4
+    },
+    {
+      "NO": 2,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "ENTERO RD-S TRIMLESS 93010 W",
+      "DEGREE": 10.0,
+      "LM": 502,
+      "W": 7
+    },
+    {
+      "NO": 3,
+      "FIRM": "FLOS",
+      "MODEL": "Light Shadow Pro Ø 90 Fixed Optic Super Spot",
+      "DEGREE": 10.0,
+      "LM": 1359,
+      "W": 18
+    },
+    {
+      "NO": 4,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "iMAX II ROUND ADJUSTABLE LP 93012 W",
+      "DEGREE": 12.0,
+      "LM": 784,
+      "W": 10
+    },
+    {
+      "NO": 5,
+      "FIRM": "FLOS",
+      "MODEL": "Light Shadow Pro Ø 90 Fixed Optic Spot",
+      "DEGREE": 14.0,
+      "LM": 1900,
+      "W": 22
+    },
+    {
+      "NO": 6,
+      "FIRM": "FLOS",
+      "MODEL": "Light Shadow Pro Ø 45 Fixed Optic Spot",
+      "DEGREE": 15.0,
+      "LM": 673,
+      "W": 8
+    },
+    {
+      "NO": 7,
+      "FIRM": "FLOS",
+      "MODEL": "Light Shadow Pro Ø 60 Fixed Optic Spot",
+      "DEGREE": 15.0,
+      "LM": 1233,
+      "W": 15
+    },
+    {
+      "NO": 8,
+      "FIRM": "FLOS",
+      "MODEL": "Kap Ø 50 Fixed Optic Spot",
+      "DEGREE": 15.0,
+      "LM": 583,
+      "W": 7
+    },
+    {
+      "NO": 9,
+      "FIRM": "LUCE LIGHT",
+      "MODEL": "COMBINA D 1.0",
+      "DEGREE": 16.0,
+      "LM": 293,
+      "W": 6
+    },
+    {
+      "NO": 10,
+      "FIRM": "LUCE LIGHT",
+      "MODEL": "ESEM MINI 2.6",
+      "DEGREE": 16.0,
+      "LM": 132,
+      "W": 2
+    },
+    {
+      "NO": 11,
+      "FIRM": "FLOS",
+      "MODEL": "Kap Ø 105 Fixed Optic Spot",
+      "DEGREE": 17.0,
+      "LM": 2272,
+      "W": 25
+    },
+    {
+      "NO": 12,
+      "FIRM": "LUCE LIGHT",
+      "MODEL": "COMBINA D 3.0",
+      "DEGREE": 18.0,
+      "LM": 1017,
+      "W": 15
+    },
+    {
+      "NO": 13,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "iMAX II ROUND ADJUSTABLE MP 93018 W",
+      "DEGREE": 18.0,
+      "LM": 1450,
+      "W": 20
+    },
+    {
+      "NO": 14,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "MINI DEEP RINGO II 93018 W",
+      "DEGREE": 18.0,
+      "LM": 700,
+      "W": 10
+    },
+    {
+      "NO": 15,
+      "FIRM": "i GUZZINI",
+      "MODEL": "LASER FIXED ROUND RA05",
+      "DEGREE": 18.0,
+      "LM": 1746,
+      "W": 17
+    },
+    {
+      "NO": 16,
+      "FIRM": "FLOS",
+      "MODEL": "Easy Kap Ø30 Fixed",
+      "DEGREE": 19.0,
+      "LM": 218,
+      "W": 3
+    },
+    {
+      "NO": 17,
+      "FIRM": "FLOS",
+      "MODEL": "Light Shadow Pro Ø 45 Fixed Optic Medium",
+      "DEGREE": 19.0,
+      "LM": 671,
+      "W": 8
+    },
+    {
+      "NO": 18,
+      "FIRM": "LUCE LIGHT",
+      "MODEL": "COMBINA D 4.0",
+      "DEGREE": 20.0,
+      "LM": 2300,
+      "W": 29
+    },
+    {
+      "NO": 19,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "ENTERO RD-S TRIMLESS 93020 W",
+      "DEGREE": 20.0,
+      "LM": 836,
+      "W": 14
+    },
+    {
+      "NO": 20,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "iMAX II ROUND ADJUSTABLE LP 93020 W",
+      "DEGREE": 20.0,
+      "LM": 845,
+      "W": 10
+    },
+    {
+      "NO": 21,
+      "FIRM": "LUCE LIGHT",
+      "MODEL": "COMBINA D 1.0",
+      "DEGREE": 22.0,
+      "LM": 290,
+      "W": 6
+    },
+    {
+      "NO": 22,
+      "FIRM": "FLOS",
+      "MODEL": "Light Shadow Pro Ø 90 Wall-Washer",
+      "DEGREE": 23.0,
+      "LM": 1336,
+      "W": 18
+    },
+    {
+      "NO": 23,
+      "FIRM": "FLOS",
+      "MODEL": "Kap Ø 50 Fixed Optic Medium",
+      "DEGREE": 24.0,
+      "LM": 524,
+      "W": 7
+    },
+    {
+      "NO": 24,
+      "FIRM": "FLOS",
+      "MODEL": "Light Shadow Pro Ø 60 Fixed Optic Medium",
+      "DEGREE": 25.0,
+      "LM": 1224,
+      "W": 15
+    },
+    {
+      "NO": 25,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "MINI DEEP RINGO II 93025 W",
+      "DEGREE": 25.0,
+      "LM": 714,
+      "W": 10
+    },
+    {
+      "NO": 26,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "iMAX II ROUND ADJUSTABLE MP 93026 W",
+      "DEGREE": 26.0,
+      "LM": 1567,
+      "W": 20
+    },
+    {
+      "NO": 27,
+      "FIRM": "FLOS",
+      "MODEL": "Easy Kap Ø30 Fixed",
+      "DEGREE": 26.0,
+      "LM": 215,
+      "W": 3
+    },
+    {
+      "NO": 28,
+      "FIRM": "FLOS",
+      "MODEL": "Kap Ø 105 Fixed Optic Medium",
+      "DEGREE": 27.0,
+      "LM": 2220,
+      "W": 22
+    },
+    {
+      "NO": 29,
+      "FIRM": "FLOS",
+      "MODEL": "Light Shadow Pro Ø 90 Fixed Optic Medium",
+      "DEGREE": 27.0,
+      "LM": 1936,
+      "W": 22
+    },
+    {
+      "NO": 30,
+      "FIRM": "LUCE LIGHT",
+      "MODEL": "ESEM MINI 1.6",
+      "DEGREE": 27.0,
+      "LM": 125,
+      "W": 2
+    },
+    {
+      "NO": 31,
+      "FIRM": "LUCE LIGHT",
+      "MODEL": "COMBINA D 3.0",
+      "DEGREE": 29.0,
+      "LM": 1017,
+      "W": 15
+    },
+    {
+      "NO": 32,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "ENTERO RD-S TRIMLESS 93030 W",
+      "DEGREE": 30.0,
+      "LM": 831,
+      "W": 14
+    },
+    {
+      "NO": 33,
+      "FIRM": "i GUZZINI",
+      "MODEL": "LASER FIXED ROUND RA06",
+      "DEGREE": 30.0,
+      "LM": 1811,
+      "W": 17
+    },
+    {
+      "NO": 34,
+      "FIRM": "FLOS",
+      "MODEL": "Light Shadow Pro Ø 45 Fixed Optic Flood",
+      "DEGREE": 32.0,
+      "LM": 692,
+      "W": 8
+    },
+    {
+      "NO": 35,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "iMAX II ROUND ADJUSTABLE LP 93033 W",
+      "DEGREE": 33.0,
+      "LM": 796,
+      "W": 10
+    },
+    {
+      "NO": 36,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "DEEP RINGO LED 93033 W",
+      "DEGREE": 33.0,
+      "LM": 803,
+      "W": 10
+    },
+    {
+      "NO": 37,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "REO 93033 S2 W",
+      "DEGREE": 33.0,
+      "LM": 918,
+      "W": 10
+    },
+    {
+      "NO": 38,
+      "FIRM": "FLOS",
+      "MODEL": "Light Shadow Pro Ø 60 Fixed Optic Flood",
+      "DEGREE": 34.0,
+      "LM": 1246,
+      "W": 15
+    },
+    {
+      "NO": 39,
+      "FIRM": "FLOS",
+      "MODEL": "Easy Kap Ø30 Fixed",
+      "DEGREE": 34.0,
+      "LM": 185,
+      "W": 3
+    },
+    {
+      "NO": 40,
+      "FIRM": "LUCE LIGHT",
+      "MODEL": "ESEM MINI 2.6",
+      "DEGREE": 36.0,
+      "LM": 122,
+      "W": 2
+    },
+    {
+      "NO": 41,
+      "FIRM": "LUCE LIGHT",
+      "MODEL": "COMBINA D 3.0",
+      "DEGREE": 36.0,
+      "LM": 1017,
+      "W": 15
+    },
+    {
+      "NO": 42,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "MINI DEEP RINGO II 93037 W",
+      "DEGREE": 37.0,
+      "LM": 678,
+      "W": 10
+    },
+    {
+      "NO": 43,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "MINI REO II X 93037 W",
+      "DEGREE": 37.0,
+      "LM": 749,
+      "W": 10
+    },
+    {
+      "NO": 44,
+      "FIRM": "LUCE LIGHT",
+      "MODEL": "COMBINA D 4.0",
+      "DEGREE": 38.0,
+      "LM": 2168,
+      "W": 29
+    },
+    {
+      "NO": 45,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "iMAX II ROUND ADJUSTABLE MP 93039 W",
+      "DEGREE": 39.0,
+      "LM": 1414,
+      "W": 20
+    },
+    {
+      "NO": 46,
+      "FIRM": "FLOS",
+      "MODEL": "Light Shadow Pro Ø 90 Fixed Optic Flood",
+      "DEGREE": 39.0,
+      "LM": 1878,
+      "W": 22
+    },
+    {
+      "NO": 47,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "ENTERO RD-S TRIMLESS 93045 W",
+      "DEGREE": 45.0,
+      "LM": 835,
+      "W": 14
+    },
+    {
+      "NO": 48,
+      "FIRM": "FLOS",
+      "MODEL": "Light Shadow Pro Ø 90 Fixed Optic Wide Flood",
+      "DEGREE": 48.0,
+      "LM": 1999,
+      "W": 22
+    },
+    {
+      "NO": 49,
+      "FIRM": "FLOS",
+      "MODEL": "Kap Ø 50 Fixed Optic Flood",
+      "DEGREE": 49.0,
+      "LM": 532,
+      "W": 7
+    },
+    {
+      "NO": 50,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "REO 93050 S2 W",
+      "DEGREE": 50.0,
+      "LM": 908,
+      "W": 10
+    },
+    {
+      "NO": 51,
+      "FIRM": "FLOS",
+      "MODEL": "Light Shadow Pro Ø 60 Fixed Optic Wide Flood",
+      "DEGREE": 51.0,
+      "LM": 995,
+      "W": 15
+    },
+    {
+      "NO": 52,
+      "FIRM": "FLOS",
+      "MODEL": "Kap Ø 105 Fixed Optic Flood",
+      "DEGREE": 51.0,
+      "LM": 2406,
+      "W": 25
+    },
+    {
+      "NO": 53,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "NIME II 93052 W",
+      "DEGREE": 52.0,
+      "LM": 437,
+      "W": 9
+    },
+    {
+      "NO": 54,
+      "FIRM": "FLOS",
+      "MODEL": "Easy Kap Ø30 Fixed",
+      "DEGREE": 53.0,
+      "LM": 169,
+      "W": 3
+    },
+    {
+      "NO": 55,
+      "FIRM": "i GUZZINI",
+      "MODEL": "LASER FIXED ROUND RA07",
+      "DEGREE": 56.0,
+      "LM": 1879,
+      "W": 17
+    },
+    {
+      "NO": 56,
+      "FIRM": "FLOS",
+      "MODEL": "Bon Jour Ø 145 Dali",
+      "DEGREE": 58.0,
+      "LM": 1530,
+      "W": 36
+    },
+    {
+      "NO": 57,
+      "FIRM": "DELTALIGHT",
+      "MODEL": "ARTUUR R 93060 W",
+      "DEGREE": 60.0,
+      "LM": 619,
+      "W": 10
+    },
+    {
+      "NO": 58,
+      "FIRM": "FLOS",
+      "MODEL": "Light Supply Fixed Trim Dimmable",
+      "DEGREE": 65.0,
+      "LM": 1158,
+      "W": 26
+    },
+    {
+      "NO": 59,
+      "FIRM": "LOUIS POULSEN",
+      "MODEL": "LP BASIC MINI",
+      "DEGREE": 65.0,
+      "LM": 847,
+      "W": 14
+    },
+    {
+      "NO": 60,
+      "FIRM": "i GUZZINI",
+      "MODEL": "BeTwoPure Ø114 - 13W",
+      "DEGREE": 65.0,
+      "LM": 1504,
+      "W": 13
+    },
+    {
+      "NO": 61,
+      "FIRM": "i GUZZINI",
+      "MODEL": "BeTwoPure Ø172 - 24W",
+      "DEGREE": 65.0,
+      "LM": 2755,
+      "W": 24
+    },
+    {
+      "NO": 62,
+      "FIRM": "i GUZZINI",
+      "MODEL": "Easy - Ø172 - 17W",
+      "DEGREE": 78.0,
+      "LM": 2160,
+      "W": 17
+    },
+    {
+      "NO": 63,
+      "FIRM": "i GUZZINI",
+      "MODEL": "Easy - Ø234 - 36W",
+      "DEGREE": 78.0,
+      "LM": 4500,
+      "W": 36
+    },
+    {
+      "NO": 64,
+      "FIRM": "LUCE LIGHT",
+      "MODEL": "ESEM MINI 2.6",
+      "DEGREE": 80.0,
+      "LM": 118,
+      "W": 2
+    },
+    {
+      "NO": 65,
+      "FIRM": "FLOS",
+      "MODEL": "Wan Downlight",
+      "DEGREE": 83.0,
+      "LM": 436,
+      "W": 9
+    },
+    {
+      "NO": 66,
+      "FIRM": "XAL",
+      "MODEL": "VELA 300 direct surface",
+      "DEGREE": 110.0,
+      "LM": 993,
+      "W": 10
+    },
+    {
+      "NO": 67,
+      "FIRM": "XAL",
+      "MODEL": "VELA 450 direct surface",
+      "DEGREE": 110.0,
+      "LM": 2050,
+      "W": 16
+    },
+    {
+      "NO": 68,
+      "FIRM": "XAL",
+      "MODEL": "VELA 600 direct surface",
+      "DEGREE": 110.0,
+      "LM": 4020,
+      "W": 31
+    },
+    {
+      "NO": 69,
+      "FIRM": "XAL",
+      "MODEL": "VELA 900 direct surface",
+      "DEGREE": 110.0,
+      "LM": 8990,
+      "W": 71
+    },
+    {
+      "NO": 70,
+      "FIRM": "LOUIS POULSEN",
+      "MODEL": "LP CIRCLE RECESSED",
+      "DEGREE": 120.0,
+      "LM": 1332,
+      "W": 17
+    },
+    {
+      "NO": 71,
+      "FIRM": "LOUIS POULSEN",
+      "MODEL": "MUNKEGAARD",
+      "DEGREE": 120.0,
+      "LM": 2145,
+      "W": 30
+    }
+  ],
+  "LineerDirekt": [
+    {
+      "NO": 1,
+      "FIRM": "FAGERHULT",
+      "MODEL": "12149-517 Closs LED Direct",
+      "DEGREE": 100.0,
+      "LM": 1972,
+      "W": 16
+    },
+    {
+      "NO": 2,
+      "FIRM": "FAGERHULT",
+      "MODEL": "12151-402 Closs LED Direct",
+      "DEGREE": 100.0,
+      "LM": 3405,
+      "W": 27
+    },
+    {
+      "NO": 3,
+      "FIRM": "FAGERHULT",
+      "MODEL": "12152 Closs LED Direct",
+      "DEGREE": 100.0,
+      "LM": 5538,
+      "W": 42
+    },
+    {
+      "NO": 4,
+      "FIRM": "FAGERHULT",
+      "MODEL": "13377-449 Notor 65 Delta",
+      "DEGREE": 100.0,
+      "LM": 3324,
+      "W": 34
+    },
+    {
+      "NO": 5,
+      "FIRM": "FAGERHULT",
+      "MODEL": "13376-449 Notor 65 Delta",
+      "DEGREE": 100.0,
+      "LM": 1550,
+      "W": 16
+    },
+    {
+      "NO": 6,
+      "FIRM": "REGENT",
+      "MODEL": "Channel S Boost Office",
+      "DEGREE": 60.0,
+      "LM": 3950,
+      "W": 40
+    },
+    {
+      "NO": 7,
+      "FIRM": "REGENT",
+      "MODEL": "Channel S Office",
+      "DEGREE": 90.0,
+      "LM": 1721,
+      "W": 20
+    },
+    {
+      "NO": 8,
+      "FIRM": "REGENT",
+      "MODEL": "Geo 3 Office - 1210X162",
+      "DEGREE": 60.0,
+      "LM": 3200,
+      "W": 26
+    },
+    {
+      "NO": 9,
+      "FIRM": "REGENT",
+      "MODEL": "Item Office-1260X325 - 44W",
+      "DEGREE": 90.0,
+      "LM": 4400,
+      "W": 44
+    },
+    {
+      "NO": 10,
+      "FIRM": "REGENT",
+      "MODEL": "Geo 4 office - 1250x250-34W",
+      "DEGREE": 100.0,
+      "LM": 3641,
+      "W": 34
+    }
+  ],
+  "LineerEndirekt": [
+    {
+      "NO": 1,
+      "FIRM": "FAGERHULT",
+      "MODEL": "DTI LED Type 2-19W",
+      "DEGREE": 120.0,
+      "LM": 2935,
+      "W": 19
+    },
+    {
+      "NO": 2,
+      "FIRM": "FAGERHULT",
+      "MODEL": "13130-402 NOTOR 78 - UP/DOWN - 70/30",
+      "DEGREE": 90.0,
+      "LM": 3399,
+      "W": 29
+    },
+    {
+      "NO": 3,
+      "FIRM": "FAGERHULT",
+      "MODEL": "13132-402 NOTOR 78 - UP/DOWN - 70/30",
+      "DEGREE": 90.0,
+      "LM": 5006,
+      "W": 43
+    },
+    {
+      "NO": 4,
+      "FIRM": "FAGERHULT",
+      "MODEL": "13502-402 NOTOR 65 Indirect",
+      "DEGREE": 120.0,
+      "LM": 2754,
+      "W": 21
+    },
+    {
+      "NO": 5,
+      "FIRM": "FAGERHULT",
+      "MODEL": "13308-449 NOTOR 65 Beta Opti",
+      "DEGREE": 90.0,
+      "LM": 4665,
+      "W": 35
+    },
+    {
+      "NO": 6,
+      "FIRM": "FAGERHULT",
+      "MODEL": "12175 Closs LED Direct/Indirect",
+      "DEGREE": 90.0,
+      "LM": 7871,
+      "W": 54
+    },
+    {
+      "NO": 7,
+      "FIRM": "FAGERHULT",
+      "MODEL": "22350-449 Dwide Pendant",
+      "DEGREE": 90.0,
+      "LM": 6592,
+      "W": 52
+    },
+    {
+      "NO": 8,
+      "FIRM": "FAGERHULT",
+      "MODEL": "11085-402 Appareo",
+      "DEGREE": 60.0,
+      "LM": 5490,
+      "W": 38
+    },
+    {
+      "NO": 9,
+      "FIRM": "FAGERHULT",
+      "MODEL": "13660-20017 Notor 65 Delta",
+      "DEGREE": 90.0,
+      "LM": 4361,
+      "W": 44
+    },
+    {
+      "NO": 10,
+      "FIRM": "FAGERHULT",
+      "MODEL": "DTI LED Type 2-38W",
+      "DEGREE": 120.0,
+      "LM": 5780,
+      "W": 38
+    }
+  ]
+};
